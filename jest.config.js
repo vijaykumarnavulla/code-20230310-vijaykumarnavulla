227db6e56bfe1162169a4277d9a1975b5228f2f1
vijaykumarnavulla <vk586072@gmail.com>
2023-03-10 22:58:51 +0530

    adding jest configartion , resolve css issue.syntx error

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,16 @@
+/** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
 module.exports = {
-    preset: 'ts-jest',
-    testEnvironment: 'jsdom',
-    collectCoverage: true,
-    collectCoverageFrom: ['src/**/*.{ts,tsx}', '!src/index.tsx'],
-    coverageDirectory: 'coverage',
-  };
+  testEnvironment: 'jest-environment-jsdom',
+  preset: 'ts-jest',
+  testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$',
+  transform: {
+    '^.+\\.tsx?$': 'ts-jest',
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'json'],
+  moduleNameMapper: {
+    '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
+      '<rootDir>/tests/__mocks__/fileMock.js',
+    '\\.(css|less)$':  "identity-obj-proxy",
+  },
+  setupFilesAfterEnv: ['@testing-library/jest-dom'],
+};
